85ea5fc7936cf8e75ce68b83c3ff5d3037263e0f
pkobzar <pkobzar@uwaterloo.ca>
2023-09-26 14:12:20 +0800

    fix routing issue

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ ReactDOM.render(
     <Navigation />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/portfolio" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/movegreen" element={<Movegreen />} />
